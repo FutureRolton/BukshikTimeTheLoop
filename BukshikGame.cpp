1b7e4f1d3910a927632b223d43eb940f67344067
MiishaG <babusha952@gmail.com>
2019-10-24 19:21:10 +0300

    not game

--- BukshikGame.cpp
+++ BukshikGame.cpp
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <SFML/Graphics.hpp>
+using namespace sf;
 int main() {
-  printf("Я ничего не знаю");
+  RenderWindow window(VideoMode(800, 600), "Bucshin Game - start");
+
+  CircleShape MyCircle(100.f);
+  MyCircle.setFillColor(Color::Blue);
+
+  while(window.isOpen()) {
+    Event event;
+    while(window.pollEvent(event)) {
+      if(event.type == Event::Closed) {
+        window.close();
+      }
+    }
+    window.clear();
+    window.draw(MyCircle);
+    window.display();
+  }
   return 0;
 }
