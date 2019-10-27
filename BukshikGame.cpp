b77ee5dd65eeebab714681dccc56fd184e4d0af3
MiishaG <babusha952@gmail.com>
2019-10-27 12:13:04 +0300

    update !+Cmake < C++

--- BukshikGame.cpp
+++ BukshikGame.cpp
@@ -5,11 +5,13 @@ using namespace sf;
 int main() {
   RenderWindow window(VideoMode(800, 600), "BukshikGame");
 
-  CircleShape shape(200.f);
-  shape.setFillColor(Color::Red);
-
+  int z = 0;
   while(window.isOpen()) {
     Event event;
+    CircleShape shape(z);
+    shape.setFillColor(Color::Red);
+    z++;
+
     while(window.pollEvent(event)) {
       if(event.type == Event::Closed) {
         window.close();
